a839893158e0371e8a8d290116b769aa8e97efc1
martinezga <52822208+martinezga@users.noreply.github.com>
2020-12-26 12:22:54 -0300

    Stage 7/8: Error_

--- hangman.py
+++ hangman.py
@@ -6,52 +6,76 @@ class HangmanGame:
         self.game_words = 'python', 'java', 'kotlin', 'javascript'
         self.random_word = ''
         self.hint = ''
-        self.lives = 7
+        self.lives = 8
+        self.letters_guessed = []
+        self.last_input = ''
 
     def get_word(self):
         self.random_word = self.game_words[random.randrange(len(self.game_words))]
         self.hint = '-' * len(self.random_word)
 
-    def check_input(self, user_input):
+    def catch_errors(self, user_input):
+        if (len(user_input) > 1) or (len(user_input) == 0):
+            print('You should input a single letter')
+            return -1
+        if not(user_input.isalpha()) or user_input.isupper():
+            print('Please enter a lowercase English letter')
+            return -1
+        self.last_input = user_input
+        return 0
+
+    def is_guessed(self):
+        for letter in self.letters_guessed:
+            if self.last_input == letter:
+                print("You've already guessed this letter")
+                return 1
+        return 0
+
+    def search_letter(self):
         hint = list(self.hint)
         flag = 0
         for i in range(len(self.random_word)):
-            if (user_input == self.hint[i]) and (user_input != '-'):
-                self.count_lives(-1)
-                flag = flag + 1
-                print('No improvements')
-                break
-            if user_input == self.random_word[i]:
-                flag = flag + 1
-                hint[i] = user_input
+            if self.last_input == self.random_word[i]:
+                flag += 1
+                hint[i] = self.last_input
         if flag == 0:
-            self.count_lives(-1)
             print("That letter doesn't appear in the word")
-        hint_str = ''
-        for letter in hint:
-            hint_str += letter
-        self.hint = hint_str
-        if self.hint == self.random_word:
-            self.lives = -1
-            print('\n' + self.hint)
-            print('You guessed the word!')
-            print('You survived!')
+            self.count_lives(-1)
+        self.letters_guessed.append(self.last_input)
+        self.list_to_str(hint)
 
     def count_lives(self, number):
         self.lives += number
-        if self.lives < 0:
+        if self.lives <= 0:
             print('You lost!')
+
+    def list_to_str(self, list_):
+        str_ = ''
+        for letter in list_:
+            str_ += letter
+        self.hint = str_
+
+    def compare_hint(self):
+        if self.hint == self.random_word:
+            self.lives = -1
+            print(f'You guessed the word {self.random_word}!')
+            print('You survived!')
+
+    def play(self):
+        if not(self.is_guessed()):
+            self.search_letter()
+            self.compare_hint()
 
 
 def main():
     game = HangmanGame()
     game.get_word()
     print('H A N G M A N')
-    while game.lives >= 0:
+    while game.lives > 0:
         print('\n' + game.hint)
         user_letter = input('Input a letter: ')
-        game.check_input(user_letter)
+        if game.catch_errors(user_letter) != -1:
+            game.play()
 
 
 main()
-
